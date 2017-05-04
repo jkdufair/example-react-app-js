65c6ec7de657fdabdaadf82d557442de28e7d037
Jason Dufair <jase@purdue.edu>
2017-05-04 18:25:10 -0400

    Use updated action names

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import { action } from './index'
 import { connect } from 'react-redux'
+import { actions as netActions } from 'studiokit-net-js'
+import { actions as authActions } from 'studiokit-auth-js'
+
 
 class App extends Component {
 	constructor(props) {
@@ -30,30 +33,30 @@ class App extends Component {
 				IsAuthenticated: { isAuthenticated === true ? 'true' : 'false' }
 				<br />
 				{isAuthenticated ? 
-					<button onClick={() => action('LOG_OUT')}>Log Out</button> :
+					<button onClick={() => action(authActions.LOG_OUT_REQUESTED)}>Log Out</button> :
 					<div>
 						<div>
-							<button onClick={() => action('CAS_LOGIN')}>CAS Login</button>
-							<button onClick={() => action('SHIB_LOGIN')}>Shibboleth Login</button>
-							<button onClick={() => action('FACEBOOK_LOGIN')}>Facebook Login</button>
+							<button onClick={() => action(authActions.CAS_LOGIN_REQUESTED)}>CAS Login</button>
+							<button onClick={() => action(authActions.SHIB_LOGIN_REQUESTED)}>Shibboleth Login</button>
+							<button onClick={() => action(authActions.FACEBOOK_LOGIN_REQUESTED)}>Facebook Login</button>
 						</div>
 						<br />
 						<div>
 							<input type='text' name='username' placeholder='username' value={username} onChange={e => this.handleInputChange(e)}/><br />
 							<input type='text' name='password' placeholder='password' value={password} onChange={e => this.handleInputChange(e)}/><br />
-							<button onClick={() => action('LOCAL_LOGIN', { payload: {
+							<button onClick={() => action(authActions.LOCAL_LOGIN_REQUESTED, { payload: {
 								'Username': username,
 								'Password': password
 							}})}>Local Login</button>
 						</div>
 					</div>}
 				<br/>
-				<button onClick={() => action('FETCH_DATA', { modelName: 'test'})}>Fetch Data</button>
-				<button onClick={() => action('FETCH_DATA_RECURRING', {	modelName: 'test2',	taskId: 'abc', period: 500 })}>Fetch Data Recurring (500ms)</button>
-				<button onClick={() => action('FETCH_DATA_CANCEL', { taskId: 'abc' })}>Cancel Recurring Fetch</button>
+				<button onClick={() => action(netActions.DATA_REQUESTED, { modelName: 'test'})}>Fetch Data</button>
+				<button onClick={() => action(netActions.PERIODIC_DATA_REQUESTED, {	modelName: 'test2',	taskId: 'abc', period: 500 })}>Fetch Data Recurring (500ms)</button>
+				<button onClick={() => action(netActions.PERIODIC_TERMINATION_REQUESTED, { taskId: 'abc' })}>Cancel Recurring Fetch</button>
 				<button onClick={() => {
 						for (var i = 0; i < 100; i++) {
-							action('FETCH_DATA_LATEST', { modelName: 'test3' })
+							action(netActions.DATA_REQUESTED_USE_LATEST, { modelName: 'test3' })
 						}
 					}}>Fetch Data Latest</button>
       </div>
