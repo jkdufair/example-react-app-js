3b026ea7af4813c85e6afc037cfda9f108fd9969
Jason Dufair <jase@purdue.edu>
2017-05-15 16:53:57 -0400

    Use local storage for token persistence

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -2,10 +2,11 @@ import { all } from 'redux-saga/effects'
 import { sagas as authSagas, apis as authApis } from 'studiokit-auth-js'
 import { sagas as netSagas } from 'studiokit-net-js'
 import apis from '../apis'
+import { tokenPersistenceService } from '../services/tokenPersistenceService'
 
 export default function* rootSaga() { 
 	yield all({
-			loginFlow: authSagas.auth({ client_id: 'web', client_secret: '7210f703-f941-4601-b293-f0c2f8102fbc'}),
-			fetchSaga: netSagas.fetch(Object.assign({}, apis, authApis), 'https://dev.academicforecast.org')
+			fetchSaga: netSagas.fetch(Object.assign({}, apis, authApis), 'https://dev.academicforecast.org'),
+			loginFlow: authSagas.auth({ client_id: 'web', client_secret: '7210f703-f941-4601-b293-f0c2f8102fbc'}, tokenPersistenceService)			
 	})
 }
---
+++ src/services/tokenPersistenceService.js
@@ -0,0 +1,13 @@
+const key = 'oauthToken'
+
+const tokenPersistenceService = {
+	getPersistedToken: () => {
+		return JSON.parse(localStorage.getItem(key));
+	},
+
+	persistToken: (token) => {
+		localStorage.setItem(key, JSON.stringify(token))
+	}
+}
+
+export { tokenPersistenceService };
