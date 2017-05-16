54bda822ad2862cd1a760de570dd250516be2d4e
Jason Dufair <jase@purdue.edu>
2017-05-16 15:36:33 -0400

    Use lifecycle events to get user info

--- src/components/Authenticated.js
+++ src/components/Authenticated.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import FlatButton from 'material-ui/FlatButton'
 import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
-import { Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
 import Snackbar from 'material-ui/Snackbar'
 import CircularProgress from 'material-ui/CircularProgress'
 import { action } from '../index'
@@ -34,127 +34,156 @@ class Authenticated extends Component {
 		}
 	}
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.auth.isAuthenticated) {
-			this.setState({whichLoginButtonClicked: undefined})
+	componentDidMount() {
+		const { auth } = this.props
+		if (auth.isAuthenticated) {
+			this.getUserInfo()
 		}
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (!this.props.auth.isAuthenticated && nextProps.auth.isAuthenticated) {
+			this.setState({ whichLoginButtonClicked: undefined })
+			this.getUserInfo()
+		}
+	}
+
+	getUserInfo() {
+		action(netActions.DATA_REQUESTED, { modelName: 'user.userInfo' })
+	}
+
 	handleInputChange(event) {
-  	const target = event.target;
-    const value = target.value;
-    const name = target.name;
+		const target = event.target
+		const value = target.value
+		const name = target.name
 
 		this.setState({
 			[name]: value
 		})
 	}
-	
-	render () {
+
+	render() {
 		const { auth, userInfo } = this.props
-		const { localUsername, localPassword, headlessCasUsername, headlessCasPassword, whichLoginButtonClicked } = this.state
+		const {
+			localUsername,
+			localPassword,
+			headlessCasUsername,
+			headlessCasPassword,
+			whichLoginButtonClicked
+		} = this.state
 		return (
 			<div>
-				<div id='main-container'>
-					<div id='action-panel'>
-						{auth.isAuthenticated &&
-							<FlatButton
-								onClick={() => action(netActions.DATA_REQUESTED, { modelName: 'user.userInfo'})}
-								label='Fetch User Info'
-								primary={true}
-								fullWidth={true} />}
-						{auth.isAuthenticated ?
-							<FlatButton
-								onClick={() => action(authActions.LOG_OUT_REQUESTED)}
-								label='Log Out'
-								primary={true}
-								fullWidth={true} />
-							:
-							<div>
-								<Card style={{...spacedCard, ...leftCard}}>
-									<CardHeader title='Standard CAS Login' />
-									<CardActions>
-										<FlatButton
-											onClick={() => window.location = 'https://www.purdue.edu/apps/account/cas/login?service=http%3A%2F%2Flocalhost:3000'}
-											label='Log In'
-											primary={true}
-											disabled={auth.isAuthenticating}
-											/>
-									</CardActions>
-								</Card>
-								<Card style={{...spacedCard, ...leftCard}}>
-									<CardHeader title='"Headless" CAS Login' />
-									<CardText>
-										<TextField
-											name='headlessCasUsername' 
-											hintText='CAS username'
-											value={headlessCasUsername}
-											onChange={e => this.handleInputChange(e)} />
-										<TextField
-											name='headlessCasPassword'
-											type='password'
-											hintText='CAS password'
-											value={headlessCasPassword}
-											onChange={e => this.handleInputChange(e)} />
-									</CardText>
-									<CardActions>
-										{auth.isAuthenticating && whichLoginButtonClicked === 'cas' ?
-											<CircularProgress size={30} style={padCircularProgress} />
-										:
+				<div id="main-container">
+					<div id="action-panel">
+						{auth.isAuthenticated
+							? <FlatButton
+									onClick={() => action(authActions.LOG_OUT_REQUESTED)}
+									label="Log Out"
+									primary={true}
+									fullWidth={true}
+								/>
+							: <div>
+									<Card style={{ ...spacedCard, ...leftCard }}>
+										<CardHeader title="Standard CAS Login" />
+										<CardActions>
 											<FlatButton
-												onClick={() => {
-													action(authActions.HEADLESS_CAS_LOGIN_REQUESTED, { payload: {
-														'Username': headlessCasUsername,
-														'Password': headlessCasPassword
-													}})
-													this.setState({whichLoginButtonClicked: 'cas'})
-												}}
-												label='Log In'
+												onClick={() =>
+													(window.location =
+														'https://www.purdue.edu/apps/account/cas/login?service=http%3A%2F%2Flocalhost:3000')}
+												label="Log In"
 												primary={true}
 												disabled={auth.isAuthenticating}
 											/>
-										}
-									</CardActions>
-								</Card>
-								<Card style={leftCard}>
-									<CardHeader title='Local Login' />
-									<CardText>
-										<TextField
-											name='localUsername'
-											hintText='local username'
-											value={localUsername}
-											onChange={e => this.handleInputChange(e)} />
-										<TextField
-											type='password'
-											name='localPassword'
-											hintText='local password'
-											value={localPassword} onChange={e => this.handleInputChange(e)} />
-									</CardText>
-									<CardActions>
-										{auth.isAuthenticating && whichLoginButtonClicked === 'local' ?
-											<CircularProgress size={30} style={padCircularProgress} />									
-										:
-											<FlatButton
-												onClick={() => {
-													action(authActions.LOCAL_LOGIN_REQUESTED, { payload: {
-														'Username': localUsername,
-														'Password': localPassword
-													}})
-													this.setState({whichLoginButtonClicked: 'local'})
-												}}
-												label='Local Login'
-												primary={true}
-												disabled={auth.isAuthenticating}
-											/>
-										}
 										</CardActions>
-								</Card>
-							</div>}					
+									</Card>
+									<Card style={{ ...spacedCard, ...leftCard }}>
+										<CardHeader title="&quot;Headless&quot; CAS Login" />
+										<CardText>
+											<TextField
+												name="headlessCasUsername"
+												hintText="CAS username"
+												value={headlessCasUsername}
+												onChange={e => this.handleInputChange(e)}
+											/>
+											<TextField
+												name="headlessCasPassword"
+												type="password"
+												hintText="CAS password"
+												value={headlessCasPassword}
+												onChange={e => this.handleInputChange(e)}
+											/>
+										</CardText>
+										<CardActions>
+											{auth.isAuthenticating &&
+												whichLoginButtonClicked === 'cas'
+												? <CircularProgress
+														size={30}
+														style={padCircularProgress}
+													/>
+												: <FlatButton
+														onClick={() => {
+															action(authActions.HEADLESS_CAS_LOGIN_REQUESTED, {
+																payload: {
+																	Username: headlessCasUsername,
+																	Password: headlessCasPassword
+																}
+															})
+															this.setState({ whichLoginButtonClicked: 'cas' })
+														}}
+														label="Log In"
+														primary={true}
+														disabled={auth.isAuthenticating}
+													/>}
+										</CardActions>
+									</Card>
+									<Card style={leftCard}>
+										<CardHeader title="Local Login" />
+										<CardText>
+											<TextField
+												name="localUsername"
+												hintText="local username"
+												value={localUsername}
+												onChange={e => this.handleInputChange(e)}
+											/>
+											<TextField
+												type="password"
+												name="localPassword"
+												hintText="local password"
+												value={localPassword}
+												onChange={e => this.handleInputChange(e)}
+											/>
+										</CardText>
+										<CardActions>
+											{auth.isAuthenticating &&
+												whichLoginButtonClicked === 'local'
+												? <CircularProgress
+														size={30}
+														style={padCircularProgress}
+													/>
+												: <FlatButton
+														onClick={() => {
+															action(authActions.LOCAL_LOGIN_REQUESTED, {
+																payload: {
+																	Username: localUsername,
+																	Password: localPassword
+																}
+															})
+															this.setState({
+																whichLoginButtonClicked: 'local'
+															})
+														}}
+														label="Local Login"
+														primary={true}
+														disabled={auth.isAuthenticating}
+													/>}
+										</CardActions>
+									</Card>
+								</div>}
 					</div>
-					<div id='results-panel'>
+					<div id="results-panel">
 						{auth.isAuthenticated &&
 							<Paper style={paperStyle} zDepth={3}>
-								<div className='json-data'>
+								<div className="json-data">
 									<pre>
 										{JSON.stringify(userInfo, null, 2)}
 									</pre>
@@ -165,18 +194,20 @@ class Authenticated extends Component {
 				<Snackbar
 					open={auth && auth.didFail}
 					message="There was a problem logging in. Please try again."
-					autoHideDuration={4000} />
+					autoHideDuration={4000}
+				/>
 			</div>
-
 		)
 	}
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
 	return {
 		auth: state.auth,
-		userInfo: state.models.user !== undefined ? state.models.user.userInfo : undefined,
-	};
-};
+		userInfo: state.models.user !== undefined
+			? state.models.user.userInfo
+			: undefined
+	}
+}
 
-export default connect(mapStateToProps)(Authenticated);
+export default connect(mapStateToProps)(Authenticated)
